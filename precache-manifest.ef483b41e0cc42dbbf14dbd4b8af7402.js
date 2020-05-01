fb6ef0b115fcce10593015c288ffcaa1f738e13c
hamed emad <hamadaemadeldine@gmail.comgit config --global user.email hamadaemadeldine@gmail.comgit config --global user.email hamadaemadeldine@gmail.com>
2020-05-01 05:04:46 +0200

    Updates

---
+++ precache-manifest.ef483b41e0cc42dbbf14dbd4b8af7402.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9634cf07cb0f94aeb84788329ec3be87",
+    "url": "/react-api-project/index.html"
+  },
+  {
+    "revision": "f1c846662a19ee52de20",
+    "url": "/react-api-project/static/css/main.9ac880c8.chunk.css"
+  },
+  {
+    "revision": "aca8fcd9653649434685",
+    "url": "/react-api-project/static/js/2.54a0c9c1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-api-project/static/js/2.54a0c9c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1c846662a19ee52de20",
+    "url": "/react-api-project/static/js/main.76f079b7.chunk.js"
+  },
+  {
+    "revision": "f8cd08817763d2bf856e",
+    "url": "/react-api-project/static/js/runtime-main.6128f9d9.js"
+  }
+]);
